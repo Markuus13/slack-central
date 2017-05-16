ef03f4e2a31708c31f09df4843f80b80e425d2a9
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-15 21:32:01 -0300

    Refactor routes and quotes actions to rails conventions.

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,7 @@
+class QuotesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get 'quotes/index'
-  post 'quotes/add'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :quotes, only: [:index, :create]
 end
