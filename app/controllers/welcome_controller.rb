eeb8ea64677d769ed182299de9edb06b1846bcda
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-15 21:29:29 -0300

    Refactor of Quotes to its own controller.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,4 @@
 class WelcomeController < ApplicationController
   def index
   end
-
-  def add_quote
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get 'welcome/index'
-  post 'welcome/add_quote'
+  get 'quotes/index'
+  post 'quotes/add'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
