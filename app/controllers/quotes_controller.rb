3a0519702f5c23a39b85f2b11ea809f299db6419
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-16 14:46:15 -0300

    Refactor in token auth.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,20 +1,23 @@
 class QuotesController < ApplicationController
-  skip_before_action :verify_authenticity_token # Only for test purposes
 
   def index
     @quotes = Quote.last(5)
   end
 
   def create
-    # test without token authentication 'D72oye7v7EFlS1PMpk25Ej0G'
+    return render json: {}, status: 403 unless valid_slack_token?
+    
     author = quote_params[:user_name]
     text = quote_params[:text]
     Quote.create!(author: author, text: text)
   end
 
   private
+  def valid_slack_token?
+    quote_params[:token] == 'D72oye7v7EFlS1PMpk25Ej0G'
+  end
+
   def quote_params
-    params.permit(:token, :team_id, :team_domain, :channel_id, :channel_name,
-                  :user_id, :user_name, :command, :text, :response_url)
+    params.permit(:token, :user_name, :text)
   end
 end
