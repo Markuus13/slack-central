bae38ab4537ddd3ea00c6cb5306d125a64a426e3
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-17 13:12:12 -0300

    Checking if assets change will solve websockets issue.

--- app/assets/javascripts/channels/quotes.js
+++
@@ -1,9 +0,0 @@
-App.quotes = App.cable.subscriptions.create('QuotesChannel', {
-  received: function(data) {
-    $("#all-quotes").append(this.renderMessage(data))
-  },
-  renderMessage: function(data) {
-    return "<p><strong>Author:</strong>" + data.author + "</p>"
-         + "<p><strong>Quote:</strong>" + data.text + "</p>";
-  }
-});
--- app/assets/javascripts/channels/welcome.js
+++ app/assets/javascripts/channels/welcome.js
@@ -5,3 +5,12 @@
 this.App = {};
 
 App.cable = ActionCable.createConsumer();
+App.quotes = App.cable.subscriptions.create('QuotesChannel', {
+  received: function(data) {
+    $("#all-quotes").append(this.renderMessage(data))
+  },
+  renderMessage: function(data) {
+    return "<p><strong>Author:</strong>" + data.author + "</p>"
+         + "<p><strong>Quote:</strong>" + data.text + "</p>";
+  }
+});
