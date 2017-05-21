40b30025c0c13118aad37d441c0c58472bde1e01
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-21 09:22:24 -0300

    Testing jquery animation.

--- app/assets/javascripts/channels/welcome.js
+++ app/assets/javascripts/channels/welcome.js
@@ -5,13 +5,25 @@
 this.App = {};
 
 App.cable = ActionCable.createConsumer();
-
+/* This code is under test. If it work fine, it will be refactored. */
 App.quotes = App.cable.subscriptions.create('QuotesChannel', {
   received: function(data) {
-    $("#quote-text").empty();
-    $("#quote-author").empty();
-    $("#quote-text").append(this.renderText(data));
-    $("#quote-author").append(this.renderAuthor(data));
+    $("#quote-text").animate({ opacity: 0 }, 400, function() {
+      $(this).empty();
+    });
+
+    $("#quote-author").animate({ opacity: 0 }, 400, function() {
+      $(this).empty();
+    });
+
+    $("#quote-text").animate({ opacity: 1 }, 500, function() {
+      $(this).append(this.renderText(data));
+    });
+
+    $("#quote-author").animate({ opacity: 1 }, 500, function() {
+      $(this).append(this.renderAuthor(data));
+    });
+
   },
   renderText: function(data) {
     return data.text;
