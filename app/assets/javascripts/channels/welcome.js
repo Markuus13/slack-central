bda5f0dfa16c13eea3ab83a2e1f9ecef8a0c4c0b
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-21 09:58:09 -0300

    Animation to show appended data.

--- app/assets/javascripts/channels/welcome.js
+++ app/assets/javascripts/channels/welcome.js
@@ -8,16 +8,19 @@ App.cable = ActionCable.createConsumer();
 /* Code under test. Will be refactored soon. */
 App.quotes = App.cable.subscriptions.create('QuotesChannel', {
   received: function(data) {
+    var ref = this;
+
     $("#quote-text").animate({ opacity: 0 }, 400, function() {
       $("#quote-text").empty();
+      $("#quote-text").append(ref.renderText(data));
+      $("#quote-text").animate({ opacity: 1 }, 500);
     });
 
     $("#quote-author").animate({ opacity: 0 }, 400, function() {
       $("#quote-author").empty();
+      $("#quote-author").append(ref.renderAuthor(data));
+      $("#quote-author").animate({ opacity: 1 }, 500);
     });
-
-    $("#quote-text").append(this.renderText(data));
-    $("#quote-author").append(this.renderAuthor(data));
   },
   renderText: function(data) {
     return data.text;
