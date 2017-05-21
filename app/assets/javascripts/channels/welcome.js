a7641d68ae2a3bc3a8e629c1f903b1b9526d239c
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-21 09:51:57 -0300

    Opacity when appeding data removed.

--- app/assets/javascripts/channels/welcome.js
+++ app/assets/javascripts/channels/welcome.js
@@ -8,17 +8,16 @@ App.cable = ActionCable.createConsumer();
 /* Code under test. Will be refactored soon. */
 App.quotes = App.cable.subscriptions.create('QuotesChannel', {
   received: function(data) {
-    var ref = this;
-
     $("#quote-text").animate({ opacity: 0 }, 400, function() {
       $("#quote-text").empty();
-      $("#quote-text").append(ref.renderText(data));
     });
 
     $("#quote-author").animate({ opacity: 0 }, 400, function() {
       $("#quote-author").empty();
-      $("#quote-text").append(ref.renderAuthor(data));
     });
+
+    $("#quote-text").append(this.renderText(data));
+    $("#quote-author").append(this.renderAuthor(data));
   },
   renderText: function(data) {
     return data.text;
