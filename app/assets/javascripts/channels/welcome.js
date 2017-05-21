9caea542bb9b1ba3c832f508fc3d314e88565cbc
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-21 08:07:01 -0300

    Refactor in way of rendering new quotes.

--- app/assets/javascripts/channels/welcome.js
+++ app/assets/javascripts/channels/welcome.js
@@ -5,13 +5,18 @@
 this.App = {};
 
 App.cable = ActionCable.createConsumer();
+
 App.quotes = App.cable.subscriptions.create('QuotesChannel', {
   received: function(data) {
-    $("#last-quote").empty();
-    $("#last-quote").append(this.renderMessage(data))
+    $("#quote-text").empty();
+    $("#quote-author").empty();
+    $("#quote-text").append(this.renderText(data));
+    $("#quote-author").append(this.renderAuthor(data));
   },
-  renderMessage: function(data) {
-    return "<p>" + data.text + "</p>"
-         + "<p>by <strong>" + data.author + "</strong></p>"
+  renderText: function(data) {
+    return data.text;
+  },
+  renderAuthor: function(data) {
+    return data.author;
   }
 });
