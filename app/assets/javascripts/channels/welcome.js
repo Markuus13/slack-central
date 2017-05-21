e16af0a375b77622d009f88a7ae9969283dc2301
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-21 09:38:06 -0300

    Fixing bind of 'this'.

--- app/assets/javascripts/channels/welcome.js
+++ app/assets/javascripts/channels/welcome.js
@@ -8,14 +8,16 @@ App.cable = ActionCable.createConsumer();
 /* Code under test. Will be refactored soon. */
 App.quotes = App.cable.subscriptions.create('QuotesChannel', {
   received: function(data) {
+    var ref = this;
+
     $("#quote-text").animate({ opacity: 0 }, 400, function() {
       $("#quote-text").empty();
-      $("#quote-text").append(this.renderText(data));
+      $("#quote-text").append(ref.renderText(data));
     });
 
     $("#quote-author").animate({ opacity: 0 }, 400, function() {
       $("#quote-author").empty();
-      $("#quote-text").append(this.renderAuthor(data));
+      $("#quote-text").append(ref.renderAuthor(data));
     });
   },
   renderText: function(data) {
