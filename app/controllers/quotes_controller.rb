3ad1b80926e6f3909cac3ee9a453502edc09cb8d
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-28 09:41:44 -0300

    Response to slack API via httparty.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,3 +1,6 @@
+# TODO Part of this controller will be refactored to /lib/slack_quotes.rb
+require 'httparty'
+
 class QuotesController < ApplicationController
   protect_from_forgery except: :create
   before_action :valid_slack_token, only: [:create]
@@ -12,7 +15,8 @@ class QuotesController < ApplicationController
     if quote.save
       ActionCable.server.broadcast 'quotes', text: quote.text,
                                              author: quote.author
-      render json: response_quote(quote_params[:user_name]), status: 200
+      success_response(params[:response_url], params[:user_name]) #TODO Replace with background job her
+      head :ok
     else
       render json: { response_type: "ephemeral", text: "Sorry, something went wrong. Please try again." }
     end
@@ -20,22 +24,25 @@ class QuotesController < ApplicationController
 
   private
   def valid_slack_token?
-    return render json: {}, status: 403 unless quote_params[:token] == ENV["SLACK_AUTH_TOKEN"]
+    render json: {}, status: 403 unless quote_params[:token] == ENV["SLACK_AUTH_TOKEN"]
   end
 
   def quote_params
     params.permit(:user_name, :text)
   end
 
-  def response_quote(user_name)
-    {
-      response_type: "in_channel",
-      text: "@#{user_name} posted a new quote!",
-      attachments: [{
-          title: "Central Slack App",
-          title_link: "https://central-slack.herokuapp.com/",
-          text: "Check this and more of our quotes at https://central-slack.herokuapp.com/quotes :smile:",
-      }]
-    }
+  def success_response(response_url, user_name)
+    HttParty.post(response_url, {
+        body: {
+          response_type: "in_channel",
+          text: "#{user_name.capitalize} posted a new quote!",
+          attachments: [{
+            title: "Central Slack App",
+            title_link: "https://central-slack.herokuapp.com/",
+            text: "Check this and more of our quotes at https://central-slack.herokuapp.com/quotes :smile:",
+          }]
+        },
+        headers: { "Content-type": 'application/json' }
+    })
   end
 end
