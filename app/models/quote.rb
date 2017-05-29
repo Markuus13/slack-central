6b016cdeb12e942a33d41c2c8ed2748f55db8bf8
Marcus Vinícius <marcus.f22@gmail.com>
2017-05-29 14:42:39 -0300

    Author validation spec created.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,4 @@
 class Quote < ApplicationRecord
   alias_attribute :user_name, :author
+  validates :author, presence: true
 end
--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Quote, type: :model do
+  subject(:valid_quote) { Quote.new(author: "marcus") }
 
   it "is valid with valid attributes" do
-    expect(Quote.new).to be_valid
+    expect(valid_quote).to be_valid
+  end
+
+  it "is not valid without an author" do
+    quote = Quote.new(author: nil)
+    expect(quote).to_not be_valid
   end
 end
