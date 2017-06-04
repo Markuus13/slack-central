26ff5cfe7aeb3da43c132a2bfb231e632e2a21a7
Marcus Vinícius <marcus.f22@gmail.com>
2017-06-04 16:21:49 -0300

    Spec POST #create when params are valid to create a quote.

--- spec/controllers/quotes_controller_spec.rb
+++ spec/controllers/quotes_controller_spec.rb
@@ -10,11 +10,22 @@ RSpec.describe QuotesController, type: :controller do
   end
 
   describe "POST #create" do
-    context "when auth token is invalid" do
-      let(:invalid_token_params) {
-        { token: "invalid_token", user_name: "user_name", text: "text" }
+    let(:valid_params) {
+      {
+        token: "valid_token",
+        user_name: "user_name",
+        text: "text",
+        response_url: "https://hooks.slack.com/commands/1234/"
       }
-
+    }
+    let(:invalid_token_params) {
+      {
+        token: "invalid_token",
+        user_name: "user_name",
+        text: "text"
+      }
+    }
+    context "when auth token is invalid" do
       it "returns a 403 FORBIDDEN status" do
         post :create, params: invalid_token_params
         expect(response).to have_http_status(:forbidden)
@@ -23,7 +34,26 @@ RSpec.describe QuotesController, type: :controller do
       it "doesn't create a new quote" do
         expect {
           post :create, params: invalid_token_params
-        }.to_not change { Quote.count }
+        }.to_not change(Quote, :count)
+      end
+    end
+
+    context "when params are valid to create a quote" do
+      around do |example|
+        ClimateControl.modify SLACK_AUTH_TOKEN: 'valid_token' do
+          example.run
+        end
+      end
+
+      it "returns a 200 OK status" do
+          post :create, params: valid_params
+          expect(response).to have_http_status(:ok)
+      end
+
+      it "creates a quote" do
+        expect {
+          post :create, params: valid_params
+        }.to change(Quote, :count).by 1
       end
     end
   end
