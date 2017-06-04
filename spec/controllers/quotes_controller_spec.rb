ba16fe91d307be4f42dae6b1afb25a99a2ef3ac8
Marcus Vinícius <marcus.f22@gmail.com>
2017-06-04 00:29:25 -0300

    POST #create with invalid token spec.

--- spec/controllers/quotes_controller_spec.rb
+++ spec/controllers/quotes_controller_spec.rb
@@ -3,9 +3,18 @@ require 'rails_helper'
 RSpec.describe QuotesController, type: :controller do
 
   describe "GET #index" do
-    it "have http status 200" do
+    it "returns a 200 OK status" do
       get :index
       expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe "POST #create" do
+    context "when auth token is invalid" do
+      it "returns a 403 FORBIDDEN status" do
+        post :create, params: { token: "invalid_token" }
+        expect(response).to have_http_status(:forbidden)
+      end
     end
   end
 end
