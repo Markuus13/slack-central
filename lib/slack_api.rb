ed976a1aab4eead7e5c727735d366ff2844414bb
urielSilva <urielbcs@gmail.com>
2017-08-21 22:56:40 -0300

    removing unused method

--- lib/slack_api.rb
+++ lib/slack_api.rb
@@ -16,19 +16,4 @@ class SlackAPI
         headers: { "Content-type": 'application/json' }
     })
   end
-
-  def self.respond_projects(content, response_url, user_name)
-    HTTParty.post(response_url, {
-        body: {
-          response_type: "in_channel",
-          text: "@#{user_name} posted a new quote!",
-          attachments: [{
-            title: "Central Slack App",
-            title_link: "https://central-slack.herokuapp.com/",
-            text: "Check this and more of our quotes at https://central-slack.herokuapp.com/quotes :smile:",
-          }]
-        }.to_json,
-        headers: { "Content-type": 'application/json' }
-    })
-  end
 end
